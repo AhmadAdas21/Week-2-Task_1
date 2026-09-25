90906fef255ccb559023e600e9928aaf40736ffa
AhmadAdas21 <ahmadnajiadas20@gmail.com>
2026-09-25 21:08:40 +0300

    edit the structure of the files in project

---
+++ Week#2-Task_1/models/customer.cs
@@ -0,0 +1,19 @@
+﻿namespace Week_2_Task_1.models
+{
+    public class customer
+    {
+        public int id
+        {
+            get;set;
+        }
+        public string name
+        {
+            get;set;
+        }
+        public string emal
+        {
+            get;set;
+        }
+
+    }
+}
---
+++ Week#2-Task_1/models/product.cs
@@ -0,0 +1,19 @@
+﻿namespace Week_2_Task_1.models
+{
+    public class product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string sku
+        {
+            get; set;
+        }
+        public float price { get; set; }
+        public int stock { get; set; }
+        public bool active
+        {
+            get;
+            set;
+        }
+    }
+}
